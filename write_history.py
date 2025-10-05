939beffd03baeb104d84e84f81ec0c5ae2396b8e
Jeannie Liu <jeannie.liu@sap.com>
2025-10-05 22:49:40 +0800

    Update write_history.py to use Gua_history directory

--- write_history.py
+++ write_history.py
@@ -1,18 +1,24 @@
 import time
+import os
 
 
 def write_history(gua):
     current_time = time.localtime()
-    file = open('gua_history.txt', 'a');
-    year = current_time.tm_year
-    month = current_time.tm_mon
-    day = current_time.tm_mday
-    hour = current_time.tm_hour
-    min = current_time.tm_min
-    sec = current_time.tm_sec
+    
+    # Ensure Gua_history directory exists
+    if not os.path.exists('Gua_history'):
+        os.makedirs('Gua_history')
+    
+    file_path = os.path.join('Gua_history', 'gua_history.txt')
+    with open(file_path, 'a') as file:
+        year = current_time.tm_year
+        month = current_time.tm_mon
+        day = current_time.tm_mday
+        hour = current_time.tm_hour
+        min = current_time.tm_min
+        sec = current_time.tm_sec
 
-    file.writelines("{}-{}-{} {}:{}:{}, {}\n".format(year, month, day, hour,min,sec, gua));
-    file.close();
+        file.write("{}-{}-{} {}:{}:{}, {}\n".format(year, month, day, hour, min, sec, gua))
 
 # if __name__ == 'main':
-# write_history('风天小畜')
+#     write_history('风天小畜')
